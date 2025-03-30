609cdd8d167b2e8f341deafbebaf75fcf80e1cbb
DArtur <strelokar@gmail.com>
2025-03-31 01:16:33 +0300

    fix vercel

--- src/app/api/jsonToExcel/route.ts
+++ src/app/api/jsonToExcel/route.ts
@@ -4,11 +4,15 @@ import ExcelJS from "exceljs";
 import fs from "fs";
 
 // Load Google Service Account credentials
+const credentialsBase64 = process.env.GOOGLE_CREDENTIALS;
+
+const credentials = credentialsBase64 && JSON.parse(Buffer.from(credentialsBase64, 'base64').toString('utf-8'));
 const auth = new google.auth.GoogleAuth({
-    credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS || "{}"),
+    credentials: credentials,
     scopes: ["https://www.googleapis.com/auth/drive"],
 });
 const drive = google.drive({ version: "v3", auth });
+
 
 export async function POST(req: NextRequest) {
     try {
